3d36aff0b6b85804ee55d9a71ac5bf0737333602
Zach <zachwax101@gmail.com>
2023-06-08 09:34:42 -0400

    completed the article

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,18 +1,33 @@
 const hre = require("hardhat");
 
-console.log(hre);
-
-async function main() {  
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello World");
-  await greeter.deployed();
+async function main() {
+  const [deployer] = await hre.ethers.getSigners();
 
   console.log(
-    `contract successfully deployed to ${greeter.address}`
+    "Deploying contracts with the account:",
+    deployer.address
   );
+
+  const Greeter = await hre.ethers.getContractFactory("Greeter");
+  const greeter = await Greeter.deploy("Hello, World!");
+
+  const Token = await hre.ethers.getContractFactory("Token");
+  const token = await Token.deploy();
+
+  const NDToken = await hre.ethers.getContractFactory("NDToken");
+  const ndToken = await NDToken.deploy("Nader Dabit Token", "NDT");
+
+  await greeter.deployed();
+  await token.deployed();
+
+  console.log("Greeter deployed to:", greeter.address);
+  console.log("Token deployed to:", token.address);
+  console.log("NDToken deployed to:", ndToken.address);
 }
 
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
